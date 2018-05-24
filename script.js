b55eb69274e0e5cdec9f8113096f440ad83a0a28
Vidall <DjangoRaijin22@gmail.com>
2018-05-24 14:45:10 -0400

    Final version
    Changes the background to gradient. Might add a drop down for direction later

---
+++ script.js
@@ -0,0 +1,49 @@
+var css = document.querySelector("h3");
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body = document.getElementById("body");
+var button = document.getElementById("random");
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function random(){
+  color1.value = getRandomColor();
+  color2.value = getRandomColor();
+  
+  setGradient();
+}
+
+
+function setInitialBack(){
+  body.style.background = "linear-gradient( to right, " + color1.value + ", " + color2.value + ")"
+}
+
+function setGradient(){
+    body.style.background = "linear-gradient( to right, " + color1.value + ", " + color2.value + ")"
+    
+    css.textContent = body.style.background + ";";
+}
+
+function displayCSS(){
+  var cssText = document.createTextNode("linear-gradient( to right, " + color1.value + ", " + color2.value + ")");
+
+  css.appendChild(cssText);
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", random);
+
+setInitialBack();
+
+displayCSS();
